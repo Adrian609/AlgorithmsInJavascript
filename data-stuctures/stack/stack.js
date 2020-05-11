6e7dea6bc4629e699cfaa0e57d7c4afbc9fafa82
Adrian609 <root@Flako-PC.localdomain>
2020-05-11 14:12:46 -0400

    adding stack implementation with postfix example

--- data-stuctures/stack/stack.js
+++ data-stuctures/stack/stack.js
@@ -33,3 +33,47 @@ let stack = new Stack();
 
 console.log(stack.isEmpty());
 console.log(stack.pop());
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.printstack());
+
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+console.log(stack.printstack());
+
+function postFixEvaluation(exp) {
+    for (let i = 0; i < exp.length; i++) {
+        let c = exp[i];
+        if (!isNaN(c)) {
+            stack.push(c - '0');
+        } else {
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+            if (val1 == "Underflow" || val2 == "Underflow") {
+
+            }
+
+            switch (c) {
+                case '+':
+                    stack.push(val2 + val1);
+                    break;
+                case '-':
+                    stack.push(val2 / val1);
+                    break;
+
+                case '/':
+                    stack.push(val2 * val1);
+                    break;
+            }
+        }
+    }
+    return stack.pop();
+}
+
+console.log(postFixEvaluation('235*+8-'));
+console.log(postFixEvaluation('23*+'));
