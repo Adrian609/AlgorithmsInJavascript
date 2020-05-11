eea314a38814682699b4e43e1246b7a756dd1018
root <root@Flako-PC.localdomain>
2020-05-11 12:11:36 -0400

    formatting clean up map.js

--- data-stuctures/map/map.js
+++ data-stuctures/map/map.js
@@ -16,30 +16,27 @@ myMap.get(keyFunc);
 
 myMap.get("a string");
 myMap.get({});
-myMap.get(function() {});
+myMap.get(function () {});
 
 /// merging to maps
 
-let first = new Map ([
-    [1 ,  'one'],
-    [2, 'two'],
-    [3 , 'three'],
-])
+let first = new Map([
+  [1, "one"],
+  [2, "two"],
+  [3, "three"],
+]);
 
-let second = new Map ([
-    [1, 'uno'],
-    [2, 'dos'],
-])
+let second = new Map([
+  [1, "uno"],
+  [2, "dos"],
+]);
 
+let merged = new Map([...first, ...second]);
 
-let merged = new Map ([...first, ...second]);
-
-
-console.log(merged.get(1)) // uno
-console.log(merged.get(2)) // dos
-console.log(merged.get(3)) // three
+console.log(merged.get(1)); // uno
+console.log(merged.get(2)); // dos
+console.log(merged.get(3)); // three
 
 console.log(merged);
 console.log(first);
 console.log(second);
-
