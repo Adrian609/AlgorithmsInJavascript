494eaddfbb9b068dfac6621dfc1bc07d348595e6
adrian609 <Rodriguez.Adrian609@gmail.com>
2020-05-27 16:51:04 -0400

    adding bubble sort

---
+++ algorithms/bubble-sort.js
@@ -0,0 +1,21 @@
+function bubbleSort(data) {
+  let temp;
+  let hasChanges = true;
+  while (hasChanges) {
+    hasChanges = false;
+    for (let i = 0; i < data.length - 1; i++) {
+      let j = i + 1;
+      if (data[i] > data[j]) {
+        hasChanges = true;
+        let swap = data[i];
+        data[i] = data[j];
+        data[j] = swap;
+      }
+    }
+  }
+  return data;
+}
+
+export default {
+  bubbleSort,
+};
